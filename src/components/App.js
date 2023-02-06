a196fcac186bee02cc4ad68e5a89aa509dc40713
Megan <esler6724@gmail.com>
2023-02-05 18:20:14 -0800

    commit tree f02dea515166bdd30b90bcf6186ec6141a885066

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Home from "../components/Home.js"
+import NavBar from "../components/NavBar.js"
+import About from "../components/About.js"
 
 function App() {
   return (
